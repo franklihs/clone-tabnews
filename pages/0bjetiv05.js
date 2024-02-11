1a861cdad050a75cf2c5bab95366953d8ecf8e1a
Franklin Hartmann Schmatz <66528402+franklihs@users.noreply.github.com>
2024-02-11 18:21:00 +0000

    adicionada a página que mostra meus objetivos com o curso e o meu futuro

---
+++ pages/0bjetiv05.js
@@ -0,0 +1,27 @@
+function listaDeObjetivos() {
+  return <h1> - [ ] Aprender a programar.</h1>;
+  return <h1> - [ ] Me tornar um programador e trabalhar com programação.</h1>;
+  return (
+    <h1>
+      {" "}
+      - [ ] Ter um emprego em que eu possa me sentir feliz enquanto trabalho e
+      que minha familia também esteja feliz enquanto eu trabalho.
+    </h1>
+  );
+  return (
+    <h1>
+      {" "}
+      - [ ] Que além do trabalho eu tenha tempo pra fazer outras coiasas que eu
+      gosto e para poder disfrutar a minha familía.
+    </h1>
+  );
+  return (
+    <h1>
+      {" "}
+      - [ ] Receber um salário que pague todas as contas e que sobre para
+      podermos viajar e investir no futuro.
+    </h1>
+  );
+}
+
+export default listaDeObjetivos;
