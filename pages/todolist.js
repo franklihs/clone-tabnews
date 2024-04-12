6dec225af221992b5b2dda8b3423901b9222b2b0
Franklin Hartmann Schmatz <66528402+franklihs@users.noreply.github.com>
2024-04-12 15:28:55 +0000

    adicionado arquivo `./pages/todolist.js` do projeto paralelo `To-Do List`

---
+++ pages/todolist.js
@@ -0,0 +1,57 @@
+import { useState } from "react";
+
+const localStorageKey = "todo_list_file";
+
+function newTask() {
+  let input = document.getElementById("text_entry");
+
+  if (!input.value) {
+    alert("Digite algo para ineserir à lista");
+  } else {
+    let valuesList = JSON.parse(localStorage.getItem(localStorageKey) || "[]");
+    valuesList.push({
+      name: input.value,
+    });
+    localStorage.setItem(localStorageKey, JSON.stringify(valuesList));
+    showValues();
+  }
+}
+
+function showValues() {
+  let valuesList = JSON.parse(localStorage.getItem(localStorageKey) || "[]");
+  let listView = document.getElementById("todo_list_view");
+  listView.inerHTML = "";
+  for (let i = 0; i < valuesList.length; i++) {
+    listView.innerHTML += `<li>${valuesList[i]["name"]}</li>`;
+  }
+}
+
+function toDoList() {
+  const appName = "Lista de tarefas";
+  let task = "";
+  let list = "array";
+
+  function addTask() {}
+
+  return (
+    <>
+      <div>
+        <h1>{appName}</h1>
+        <p>
+          <input
+            type="text"
+            id="text_entry"
+            placeholder="Digite sua tarefa aqui"
+          ></input>
+          {task} <button onClick={newTask}> + </button>
+        </p>
+        <button onClick={addTask}> v </button>
+      </div>
+      <div>
+        <lo id="todo_list_view"></lo>
+      </div>
+    </>
+  );
+}
+
+export default toDoList;
