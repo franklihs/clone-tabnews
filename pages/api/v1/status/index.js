fd57217dea9e2920329b87a8e6ca9fff05633454
Franklin Hartmann Schmatz <66528402+franklihs@users.noreply.github.com>
2024-04-17 12:43:05 +0000

    finalizado o endpoint `/api/v1/status`.

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,29 @@
 import database from "infra/database.js";
+import { handleSmoothScroll } from "next/dist/shared/lib/router/router";
+import { Query } from "pg";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log(result.rows);
-  response.status(200).json({ chave: "são acima da média" });
+  const updatedAt = new Date().toISOString();
+  const postgresVersion = await database.query("SHOW server_version;");
+  const maxConnections = await database.query("SHOW MAX_CONNECTIONS;");
+
+  const databaseName = process.env.POSTGRES_DB;
+  const connections = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+  const openedConnections = connections.rows[0].count;
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: postgresVersion.rows[0].server_version,
+        max_connections: parseInt(maxConnections.rows[0].max_connections),
+        opened_connections: openedConnections,
+      },
+    },
+  });
 }
 
 export default status;
